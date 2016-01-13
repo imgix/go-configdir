eb730ceba394c5cf30c161f67c423cd307c82f42
david birdsong <david.birdsong@gmail.com>
2016-01-12 20:36:35 -0800

    initial

---
+++ bytes.go
@@ -0,0 +1,111 @@
+package dirnotify
+
+import (
+	"gopkg.in/fsnotify.v1"
+
+	"bytes"
+	"crypto/md5"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Printer interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+	Println(...interface{})
+}
+
+var logger Printer
+
+func DirectoryBytesUpdates(dir, suffix string, p Printer) (chan *bytes.Buffer, error) {
+	if p != nil {
+		logger = p
+	} else {
+		logger = log.New(os.Stderr, "[dircfg]",
+			log.Ldate|log.Ltime|log.Lshortfile,
+		)
+
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+	err = watcher.Add(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	dirBytesCh := make(chan *bytes.Buffer)
+
+	go func() {
+		var (
+			dirBytesB          *bytes.Buffer
+			currentMD5, tmpMD5 []byte
+		)
+		dirBytesB, tmpMD5 = bytesFromDir(dir, suffix)
+		currentMD5 = make([]byte, len(tmpMD5))
+		copy(currentMD5, tmpMD5)
+		dirBytesCh <- dirBytesB
+
+		evI := func(o fsnotify.Op) bool {
+			switch {
+			case o&fsnotify.Write == fsnotify.Write:
+				return true
+			case o&fsnotify.Rename == fsnotify.Rename:
+				return true
+
+			}
+
+			return false
+		}
+		updates := 0
+
+		for {
+			select {
+			case ev := <-watcher.Events:
+
+				if strings.HasSuffix(ev.Name, suffix) && evI(ev.Op) {
+					dirBytesB, tmpMD5 = bytesFromDir(dir, suffix)
+				}
+			case err := <-watcher.Errors:
+				logger.Println("error:", err)
+			}
+			if !bytes.Equal(tmpMD5, currentMD5) {
+				copy(currentMD5, tmpMD5)
+				dirBytesCh <- dirBytesB
+				updates++
+
+			} else {
+				logger.Println("ignoring no-op byte changes")
+			}
+		}
+	}()
+	return dirBytesCh, nil
+}
+
+func bytesFromDir(dir, suffix string) (*bytes.Buffer, []byte) {
+
+	var buf *bytes.Buffer = &bytes.Buffer{}
+	md5 := md5.New()
+
+	files, _ := ioutil.ReadDir(dir)
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), suffix) {
+
+			if b, err := ioutil.ReadFile(filepath.Join(dir, f.Name())); err != nil {
+				logger.Println(err)
+				logger.Printf("unable tor read file: %s, '%s', skipping", f.Name(), err)
+			} else {
+				buf.Write(b)
+				md5.Write(b)
+			}
+
+		}
+	}
+	return buf, md5.Sum(nil)
+
+}
---
+++ bytes_test.go
@@ -0,0 +1,133 @@
+package dirnotify
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func init() {
+	logger = log.New(os.Stderr, "[HwyServer-Worker]",
+		log.Ldate|log.Ltime|log.Lshortfile,
+	)
+
+}
+
+func writeManyFiles(t *testing.T, d, suffix string, numfiles int, body []byte) []byte {
+	var (
+		fname string
+		buf   *bytes.Buffer
+	)
+	buf = &bytes.Buffer{}
+	for x := 0; x < numfiles; x++ {
+		bufBody := bytes.NewBuffer(body)
+
+		// gotta zero pad so dirctory sorting returns the same
+		fname = filepath.Join(d, fmt.Sprintf("test_%06d.%s", x, suffix))
+
+		bufBody.Write([]byte(fname))
+		if err := ioutil.WriteFile(fname, bufBody.Bytes(), os.ModePerm); err != nil {
+			t.Fatal(err)
+		}
+		buf.Write(bufBody.Bytes())
+
+	}
+	return buf.Bytes()
+
+}
+
+func cleanupDir(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(dir)
+	defer d.Close()
+
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func TestBytesFromDir(t *testing.T) {
+	d, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+
+	}
+	defer cleanupDir(d)
+	wroteBytes := writeManyFiles(t, d, "toml", 20, []byte("dir files data"))
+	readBytes, readmd5 := bytesFromDir(d, "toml")
+
+	wrotemd5 := md5.New()
+	wrotemd5.Write(wroteBytes)
+
+	if !bytes.Equal(wrotemd5.Sum(nil), readmd5) {
+
+		msg := fmt.Sprintf("wrote bytes: \n%s\nread bytes:\n%s", wroteBytes, readBytes.Bytes())
+		t.Error(msg)
+	}
+
+}
+
+func TestDirectoryBytesUpdates(t *testing.T) {
+	d, err := ioutil.TempDir("", "")
+
+	if err != nil {
+		t.Fatal(err)
+
+	}
+	defer cleanupDir(d)
+	writeManyFiles(t, d, "toml", 20, []byte("initial"))
+
+	chBytes, err := DirectoryBytesUpdates(d, "toml", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	<-chBytes
+
+	for x := 0; x < 5; x++ {
+		fname := filepath.Join(d, fmt.Sprintf("test_%06d.toml", x))
+		if err := ioutil.WriteFile(fname, []byte("foo"), os.ModePerm); err != nil {
+			t.Fatal(err)
+		}
+
+		b := <-chBytes
+		bfromDir, _ := bytesFromDir(d, "toml")
+
+		if !bytes.Equal(b.Bytes(), bfromDir.Bytes()) {
+			t.Errorf("directory update returns inconsistent data")
+		}
+
+	}
+	time.Sleep(1 * time.Second)
+
+	// test no-op
+	fname := filepath.Join(d, fmt.Sprintf("test_%06d.toml", 6))
+	if err := ioutil.WriteFile(fname, []byte("foo"), os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	select {
+
+	case <-chBytes:
+		t.Errorf("md5 check let no-op file write slip through")
+	default:
+
+	}
+
+}
